e6edebb90b8c73ee768770f6cbff88d10d120101
Johan Ibrahim <johan@ms.rcsiucd.edu.my>
2024-04-23 17:19:30 +0800

    :wrench:Trying new layout

--- start_here.R
+++ start_here.R
@@ -1,7 +1,7 @@
 datadrivencv::use_datadriven_cv(
   full_name = "Dr Johan Bin Ibrahim",
-  data_location = "https://docs.google.com/spreadsheets/d/17D2pWqUjeWcpXfDgAwBTbis560FqDZLbjyXAzdLiEuo/edit#gid=917338460",
-  pdf_location = here::here(),
-  html_location = here::here(),
-  source_location = here::here()
+  data_location = "https://docs.google.com/spreadsheets/d/1_Y3HPpenl9Q0rovGZUmto6kxSCrtv8BsTGSfdk-xdXk/edit?usp=sharing",
+  pdf_location = "drjohan.github.io/johancracv/johancv.pdf",
+  html_location = "drjohan.github.io/johancracv/index.html",
+  source_location = "drjohan.github.io/johancracv"
 )
