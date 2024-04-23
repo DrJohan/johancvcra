590f71014f71139cc2559458e7a25fdf8c1354d6
Johan Ibrahim <johan@ms.rcsiucd.edu.my>
2024-04-23 13:59:16 +0800

    New CV

---
+++ start_here.R
@@ -0,0 +1,7 @@
+datadrivencv::use_datadriven_cv(
+  full_name = "Dr Johan Bin Ibrahim",
+  data_location = "https://docs.google.com/spreadsheets/d/17D2pWqUjeWcpXfDgAwBTbis560FqDZLbjyXAzdLiEuo/edit#gid=917338460",
+  pdf_location = here::here(),
+  html_location = here::here(),
+  source_location = here::here()
+)
