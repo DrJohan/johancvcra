b23853c28c99e804ce309f9e6b8fc78d9f1ee30b
DrJohan <johan@ms.rcsiucd.edu.my>
2024-12-06 11:55:27 +0800

    Minimal changes

--- start_here.R
+++ start_here.R
@@ -1,7 +1,9 @@
-datadrivencv::use_datadriven_cv(
+library(datadrivencv)
+library(here)
+use_datadriven_cv(
   full_name = "Dr Johan Bin Ibrahim",
   data_location = "https://docs.google.com/spreadsheets/d/1_Y3HPpenl9Q0rovGZUmto6kxSCrtv8BsTGSfdk-xdXk/edit?usp=sharing",
-  pdf_location = "https://github.com/DrJohan/johancvcra/drjohan_cv.pdf",
+  pdf_location = "https://github.com/DrJohan/johancvcra/raw/main/drjohan_cv.pdf",
   html_location = "drjohan.github.io/johancracv/index.html",
   source_location = "drjohan.github.io/johancracv"
 )
